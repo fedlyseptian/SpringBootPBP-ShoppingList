704c461ade93232a986e978c291c271fe8091220
fedlyseptian <fedlyseptian.fs@gmail.com>
2021-03-29 11:18:17 +0700

    Commit update repository DaftarBelanjaRepo

--- src/main/java/shoppinglist/repository/DaftarBelanjaRepo.java
+++ src/main/java/shoppinglist/repository/DaftarBelanjaRepo.java
@@ -8,9 +8,15 @@ package shoppinglist.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shoppinglist.entity.DaftarBelanja;
 
+import java.util.List;
+
 /**
- * @author irfin
+ *
+ * @author fedlyseptian
  */
+
 public interface DaftarBelanjaRepo extends JpaRepository<DaftarBelanja, Long>
 {
+    List<DaftarBelanja> findByJudulIgnoreCaseContaining(String judul);
+    DaftarBelanja findTopByOrderByIdDesc();
 }
